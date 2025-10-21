1ca3a1bb1a7fd1f3d51b00e3dcc53f876074274b
ingeun <justin-j@itclab.co.kr>
2025-10-21 17:57:59 +0900

    2025.10.21 update

--- src/common/components/MainMenu.tsx
+++ src/common/components/MainMenu.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Link from "next/link";
 
-import { JSX, useEffect, useState } from "react";
+import { JSX, useEffect, useState,useRef, TouchEvent, MouseEvent } from "react";
 import './MainMenu.scss';
 
 type NavigationItemProps = {
@@ -11,24 +11,113 @@ type NavigationItemProps = {
 
 
 export default function  MainMenu({ menu, submenu }: NavigationItemProps): JSX.Element {
- 
+
+    const scrollRef = useRef<HTMLUListElement>(null);
     const [menuIndex, setMenuIndex] = useState<number>(0);
+    const [isDragging, setIsDragging] = useState(false);
+    const [startX, setStartX] = useState(0);
+    const [scrollLeft, setScrollLeft] = useState(0);
+
     
+    // 마우스 이벤트
+    const handleMouseDown = (e: MouseEvent<HTMLUListElement, globalThis.MouseEvent>) => {
+        if (!scrollRef.current) return;
+        setIsDragging(true);
+        setStartX(e.pageX - scrollRef.current.offsetLeft);
+        setScrollLeft(scrollRef.current.scrollLeft);
+    };
+
+    const handleMouseUp = () => setIsDragging(false);
+    const handleMouseLeave = () => setIsDragging(false);
+
+    const handleMouseMove = (e: MouseEvent<HTMLUListElement, globalThis.MouseEvent>) => {
+        if (!isDragging || !scrollRef.current) return;
+        e.preventDefault();
+        const x = e.pageX - scrollRef.current.offsetLeft;
+        const walk = (x - startX) * 1.5;
+        scrollRef.current.scrollLeft = scrollLeft - walk;
+    };
+
+    // 터치 이벤트
+    const handleTouchStart = (e:TouchEvent<HTMLUListElement>) => {
+        if (!scrollRef.current) return;
+        setIsDragging(true);
+        setStartX(e.touches[0].pageX - scrollRef.current.offsetLeft);
+        setScrollLeft(scrollRef.current.scrollLeft);
+    };
+
+    const handleTouchMove = (e: TouchEvent<HTMLUListElement>) => {
+        if (!isDragging || !scrollRef.current) return;
+        const x = e.touches[0].pageX - scrollRef.current.offsetLeft;
+        const walk = (x - startX) * 1.5;
+        scrollRef.current.scrollLeft = scrollLeft - walk;
+    };
+
+    const handleTouchEnd = () => setIsDragging(false);
+
+    const menuClick = (e: MouseEvent<HTMLLIElement, globalThis.MouseEvent>, index:number) => {
+        e.stopPropagation();
+        e.preventDefault
+        if (!scrollRef.current) return;
+        if(menuIndex===index) return;
+        if(index === 0) {
+            scrollRef.current.scrollLeft = 0;
+        } else if (index === 1){
+               scrollRef.current.scrollLeft = 80;
+        }else {
+        const clickX = e.pageX - scrollRef.current.offsetLeft;
+        scrollRef.current.scrollLeft = clickX;
+        }
+
+        setMenuIndex(index)
+    };
+
+    const scrollMoveLeft = () => {
+        if (scrollRef.current) {
+        scrollRef.current.scrollBy({ left: -200, behavior: 'smooth' });
+        }
+    };
+
+    const scrollMoveRight = () => {
+        if (scrollRef.current) {
+        scrollRef.current.scrollBy({ left: 200, behavior: 'smooth' });
+        }
+    };
+
+
+
     return (
         <>
             <div className="main_menu">
                 <div className="menu_section">
-                    {menu.map((item, index) => (
-                        <ul key={index} className="menu_item">
-                            <li className={menuIndex === index ? "active": ""}
-                                onClick={() => setMenuIndex(index)}
-                            >{item}</li>
-                        </ul>
-                    )) }
+                    <ul  ref={scrollRef} 
+                        onMouseDown={handleMouseDown}
+                        onMouseLeave={handleMouseLeave}
+                        onMouseUp={handleMouseUp}
+                        onMouseMove={handleMouseMove}
+                        onTouchStart={handleTouchStart}
+                        onTouchMove={handleTouchMove}
+                        onTouchEnd={handleTouchEnd}>
+
+                        <div className="move-btn">
+                            <button  onClick={scrollMoveLeft} className="scroll-btn" aria-label="왼쪽으로 이동">
+                                <svg viewBox="0 0 24 24"><path d="M15 19l-7-7 7-7"/></svg>
+                            </button>
+                            <button onClick={scrollMoveRight} className="scroll-btn" aria-label="오른쪽으로 이동">
+                                <svg viewBox="0 0 24 24"><path d="M9 5l7 7-7 7"/></svg>
+                            </button>
+                        </div>
+
+                        {menu.map((item, index) => (
+                            <li key={index} className={menuIndex === index ? "menu_item active": "menu_item"}
+                                onClick={(e) => menuClick(e,index)}>
+                                    {item}
+                            </li>
+                        )) }
+                    </ul>
                 </div>
                 <div>
                     {submenu.map((nav, index) => {
-                       
                         if(index === menuIndex) return (
                             <div key={index} className="sub_menu">
                                 {nav.children.map((child) => (
--- src/common/define/navigation.ts
+++ src/common/define/navigation.ts
@@ -2,35 +2,19 @@
 // output: "export" 에서는 동적 파라미터가 불가능하기 때문에
 // 허용할 segment 목록을 미리 지정해야 함
 
-export const Sections:string[]= ["front","back","infra"] as const;
-export const ALLOWED_SEGMENTS_front:string[] = ["ui","builder","core","module","react"] as const;
+export const Sections:string[]= ["front","back", "db", "ai", "infra", "oss"] as const;
+export const ALLOWED_SEGMENTS_front:string[] = ["core", "module", "ui", "buildTools","react","next"] as const;
 
-export const navigation = [
-    {
-        children: ALLOWED_SEGMENTS_front.map(seg => (
-            { path :`/front/${seg}`, name: seg, key : seg, active: true}
-        )),
-    },
-    {
+export const navigation = Sections.map(index => {
+
+    if (index === "front") {
+        return {
+            children: ALLOWED_SEGMENTS_front.map(seg => ({ path :`/front/${seg}`, name: seg, key : seg, active: true}))
+        }
+    } 
+    return {
         children: []
-    },
-    {
-        children: []
-    },
-    // {
-    //     children: [
-    //         { path :'/back/java', name: "Java", key : 'java' , active: true},
-    //         { path :'/back/python', name: "Python", key : 'python'},
-    //         { path :'/back/node', name: "Node", key : 'node' , active: true},
-    //         { path :'/back/nest', name: "Nest", key : 'nest', active: true},
-    //     ]
-    // },
-    //  {
-    //     children: [
-    //         { path :'/infra/nginx', name: "Nginx", key : 'nginx' , active: true},
-    //         { path :'/infra/docker', name: "Docker", key : 'docker'},
-    //         { path :'/infra/kubernetes', name: "Kubernetes", key : 'kubernetes' , active: true},
-    //         { path :'/infra/redis', name: "Reids", key : 'redis', active: true},
-    //     ]
-    // }
-];      
+    }
+})
+
+    
